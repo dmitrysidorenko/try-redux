7efa5dc6a617803b1b30f108f95fc43f4a938182
Dmytro Sydorenko <dsydorenko@intropro.com>
2015-10-13 13:59:45 +0300

    UPD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'angular';
 import './app.js';
 import './core/index.js';
 
-import './components/menu/index.js';
+import './components/main-menu/index.js';
 import './components/menu-item/index.js';
 import './components/content/index.js';
 
